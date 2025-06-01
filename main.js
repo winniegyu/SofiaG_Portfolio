2bb75fed86faf20a010c12e564caf0d3ec274389
Joseph_MCV <214265856+winniegyu@users.noreply.github.com>
2025-06-01 18:39:05 -0500

    fixed some buges for the index

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.querySelector('.carrusel-button.next');
     let currentSlide = 0;
 
-    // Ocultar todos los slides primero
+   
     slides.forEach(slide => slide.classList.remove('active'));
-    // Mostrar el primer slide
+
     if (slides.length > 0) {
         slides[0].classList.add('active');
     }
 
     function showSlide(index) {
-        // Ocultar el slide actual
+        
         slides[currentSlide].classList.remove('active');
         
-        // Calcular el nuevo índice
+        
         currentSlide = index;
         if (currentSlide >= slides.length) currentSlide = 0;
         if (currentSlide < 0) currentSlide = slides.length - 1;
         
-        // Mostrar el nuevo slide
+       
         slides[currentSlide].classList.add('active');
     }
 
